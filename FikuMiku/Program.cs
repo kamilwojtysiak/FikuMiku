5aa6364f3264d1de69b6e4930d099fd39e6a0b0f
kamilwojtysiak <kamil@futrzane.pl>
2020-06-23 09:17:31 +0200

    FikuMiku

---
+++ FikuMiku/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace FikuMiku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enumerable.Range(1, 100).ToList()
+                .ForEach(x =>
+                {
+                    Console.WriteLine("{0}{1}{2}",
+                        x % 3 == 0 ? "Fiku" : "",
+                        x % 5 == 0 ? "Miku" : "",
+                        x % 5 != 0 && x % 3 != 0 ? x.ToString() : "");
+                });
+        }
+    }
+}
